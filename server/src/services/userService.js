0f7fea46ef98a5e37386983044f36369f11a8403
Maxim <101882533+ballinlikeme@users.noreply.github.com>
2022-12-10 03:57:33 +0300

    fix: api error recognition

--- server/src/services/userService.js
+++ server/src/services/userService.js
@@ -6,16 +6,16 @@ const ApiError = require('../exceptions/apiError');
 class UserService {
     async login(email, password) {
         const user = await User.findOne({where: {email}});
-        if (!user) throw new ApiError.BadRequest('Invalid email');
+        if (!user) throw ApiError.BadRequest('Invalid email');
         const comparePasswords = await bcrypt.compare(password, user.password)
-        if (!comparePasswords) throw new ApiError('Invalid password')
+        if (!comparePasswords) throw ApiError.BadRequest('Invalid password')
         const token = tokenService.generateAccessToken(email, user.id);
         return token;
     }
 
     async register(email, password, role) {
         const candidate = await User.findOne({where: {email}});
-        if (candidate) throw new ApiError.BadRequest('User with this email already exists')
+        if (candidate) throw ApiError.BadRequest('User with this email already exists')
         const hashedPassword = await bcrypt.hash(password, 5);
         const user = await User.create({email, password: hashedPassword, role});
         const token = tokenService.generateAccessToken(email, user.id);
@@ -25,7 +25,7 @@ class UserService {
 
     async check(token) {
         const decoded = tokenService.validateToken(token);
-        if (!decoded) throw new ApiError.UnauthorizedError();
+        if (!decoded) throw ApiError.UnauthorizedError();
         return decoded;
     }
 }
