897bde0d2c6a9c6948cdba1a0a30220a6bd7d9ea
Maxim <101882533+ballinlikeme@users.noreply.github.com>
2022-12-15 06:23:07 +0300

    fix: description storage

--- server/src/database/models/posts.js
+++ server/src/database/models/posts.js
@@ -4,7 +4,7 @@ const {DataTypes} = require('sequelize');
 const Posts = sequelize.define("Posts", {
     id: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
     title: {type: DataTypes.STRING, unique: true},
-    description: {type: DataTypes.STRING},
+    description: {type: DataTypes.TEXT},
     text: {type: DataTypes.TEXT, allowNull: false},
     author: {type: DataTypes.STRING}
 });
